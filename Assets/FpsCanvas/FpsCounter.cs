eeeb31836582e64f60388ba6db2dd5e035394485
youten <youtenredo@gmail.com>
2015-05-06 16:35:34 +0900

    1st commit

---
+++ Assets/FpsCanvas/FpsCounter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FpsCounter : MonoBehaviour {
+	private const float INTERVAL = 1.0f;
+	public Text FpsText;
+	private int Frames  = 0;
+	private float oldTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		oldTime = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frames++;
+		float ellapsed = Time.realtimeSinceStartup - oldTime;
+		if (ellapsed > INTERVAL) {
+			float fps  = Frames / ellapsed;
+			FpsText.text = "Fps " + fps.ToString("#.00");
+			oldTime = Time.realtimeSinceStartup;
+			Frames = 0;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# FpsConter
+
+For Unity 5.x, Simple FpsCounter using uGUI.
+
+# License
+
+Apache License Version 2.0
+
